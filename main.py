da8693f7943634cfc44694d4c29f6bdb05254ade
Joshua Gilgallon <131416790+JoshuaGilgallon@users.noreply.github.com>
2024-03-04 13:07:48 +0800

    Update main.py
    Removed key stroke reader and used inputs instead. Finished basic menubar structure

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
-from pynput import keyboard
 
 
 
 def menuBar():
-    print('- Select one of the 5 modules below:')
-    print('- Compare simple and compound interest savings acccounts')
-    print('- Calculate the time for a CI savings account to reach a target amount')
-    print('- Compare two Compound Interest savings accounts')
-    print('- Model a CI savings account with regular deposits')
-    print('- Model increases in compounding frequency')
-    print('- Exit')
-    with keyboard.Events() as events:
-        event = events.get(16)
-        if event.key == keyboard.KeyCode.from_char('down'):
-            print("YES")
+    print('1 - Select one of the 5 modules below:')
+    print('2 - Compare simple and compound interest savings acccounts')
+    print('3 - Calculate the time for a CI savings account to reach a target amount')
+    print('4 - Compare two Compound Interest savings accounts')
+    print('5 - Model a CI savings account with regular deposits')
+    print('6 - Model increases in compounding frequency')
+    print('7 - Exit')
+    selection = input('Select an option: ')
+    try: int(selection)
+    except: 
+        print('Please select a valid option.')
+        menuBar()
+    else:
+        return selection
     
-menuBar()
+module = menuBar()
