a0de3ec384123dc8fd1a4b302ee4f0b466f07620
Joshua Gilgallon <131416790+JoshuaGilgallon@users.noreply.github.com>
2024-03-04 13:44:45 +0800

    Update main.py
    Cleaned up code + added a more complex and efficient engine for managing inputs.

--- main.py
+++ main.py
@@ -1,66 +1,82 @@
 import os
+import sys
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 cls()
 
-def checkInt(lowVal: float, highVal: float, errorMsg: str, question: str, priorMsg: str):
-    print(priorMsg)
+def checkInt(lowVal, highVal, errorMsg: str, question: str, priorMsg: str, clearPrior: int):
+    # set lowVal and highVal, or priorMsg to 'x' if they arent needed
+    # set clearPrior to 0 if clearing output is not necessary, or 1 if it is.
+    if priorMsg != 'x':
+        print(priorMsg)
     answer = input(question)
     try:
         answer = int(answer)
-    except ValueError:
+    except:
         cls()
         print(f'{errorMsg}\n')
-        return checkInt(lowVal, highVal, errorMsg, question, priorMsg)
+        return checkInt(lowVal, highVal, errorMsg, question, priorMsg, clearPrior)
     else:
-        if lowVal < answer < highVal:
+        if lowVal == 'x':
             return answer
         else:
-            cls()
-            print(f'{errorMsg}\n')
-            return checkInt(lowVal, highVal, errorMsg, question, priorMsg)
+            if lowVal < answer < highVal:
+                return answer
+            else:
+                cls()
+                print(f'{errorMsg}\n')
+                return checkInt(lowVal, highVal, errorMsg, question, priorMsg, clearPrior)
 
 def menuBar():
-    # print('1 - Select one of the 5 modules below:')
-    # print('2 - Compare simple and compound interest savings acccounts')
-    # print('3 - Calculate the time for a CI savings account to reach a target amount')
-    # print('4 - Compare two Compound Interest savings accounts')
-    # print('5 - Model a CI savings account with regular deposits')
-    # print('6 - Model increases in compounding frequency')
-    # print('7 - Exit')
-    # selection = input('Select an option: ')
-    # try: selection = int(selection)
-    # except: 
-    #     cls()
-    #     print('>> Please select a valid option.\n')
-    #     menuBar()
-    # else:
-    #     if selection > 0 and selection < 8:
-    #         return selection
-    #     else:
-    #         cls()
-    #         print('>> Please select a valid option.\n')
-    #         menuBar()
-    checkInt(0, 8, '>> Please select a valid option.', 'Select an option: ',
+    module = checkInt(0, 8, '>> Please select a valid option.', 'Select an option: ',
             '1 - Select one of the 5 modules below:\n'
             '2 - Compare simple and compound interest savings acccounts\n'
-            '3 - Calculate the time for a CI savings account to reach a target amount\n'
+            '3 - Calculate the time for a CI savings account to re`ach a target amount\n'
             '4 - Compare two Compound Interest savings accounts\n'
             '5 - Model a CI savings account with regular deposits\n'
             '6 - Model increases in compounding frequency\n'
-            '7 - Exit'
+            '7 - Exit', 1
             )
+    
+    module_functions[module]()
 
 def moduleOne():
     cls()
     print('Module 1: Compare Simple and Compound Interest Accounts \n')
     print('Simple interest account:')
     principleAmount = input('Enter the principle amount: ')
+    checkInt('x', 'x', '>> Please enter a valid number', 'Enter the principle amount: ', 'x')
 
+def moduleTwo():
+    pass
 
+def moduleThree():
+    pass
+
+def moduleFour():
+    pass
+
+def moduleFive():
+    pass
+
+def moduleSix():
+    pass
+
+def exitProgram():
+    print('Thanks for playing!')
+    sys.exit()
     
-module = menuBar()
-
+module_functions = {
+    1: moduleOne,
+    2: moduleTwo,
+    3: moduleThree,
+    4: moduleFour,
+    5: moduleFive,
+    6: moduleSix,
+    7: exitProgram
+}
+    
+menuBar()
 
